80eed02bd070ee3f7101721b396ad7844668eafb
Yoan Kalafirov <102053097+Yoan05@users.noreply.github.com>
2023-12-09 20:07:01 +0200

    Add files via upload

---
+++ techStore/src/main/java/com/softuni/projectForExam/techStore/ParaHuntersApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.projectForExam.techStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaHuntersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaHuntersApplication.class, args);
+	}
+
+}
